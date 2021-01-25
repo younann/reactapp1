395dbff356ce29ecf7e3f92cc7e8eee07fb9f433
younann <younan.n@gmail.com>
2021-01-25 17:39:06 +0200

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
+    <div className="container">
+      <h1 className="text-center text-alert-danger">under construction</h1>
+      <img
+        alt="under construction"
+        src="https://cdn.xingosoftware.com/elektor/images/fetch/dpr_1/https%3A%2F%2Fwww.elektormagazine.com%2Fassets%2Fupload%2Fimages%2F8%2F20190629113654_Elektor-Under-Construction.png"
+      />
     </div>
   );
 }
